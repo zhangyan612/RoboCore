114c2db9089595cf9aef2192fac0d6186e2235e3
Yan Zhang <yan.zhang@insureon.com>
2022-08-21 02:01:35 +0800

    speech recognition

---
+++ src/speech-alone.py
@@ -0,0 +1,17 @@
+# importing libraries 
+# pip3 install SpeechRecognition pydub
+import speech_recognition as sr 
+import os 
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# create a speech recognition object
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
